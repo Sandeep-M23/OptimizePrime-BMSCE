28b9e92716f68cba5ff7479a1ddf30fd3450ba64
Varun S Athreya <varun.sathreya@gmail.com>
2021-11-26 17:47:14 +0530

    Created development branch

---
+++ pages/talktoexperts.tsx
@@ -0,0 +1,13 @@
+import { Text } from '@chakra-ui/react';
+import { NextPage } from 'next';
+import React from 'react';
+
+const TalkToExperts: NextPage = () => {
+    return (
+        <div>
+            <Text>Talk to Experts</Text>
+        </div>
+    );
+};
+
+export default TalkToExperts;
