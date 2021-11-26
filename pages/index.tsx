11d6f59ca6530e9ba4f1066ce5b181c640b1c279
Varun S Athreya <varun.sathreya@gmail.com>
2021-11-26 16:37:50 +0530

    Initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from '@chakra-ui/react';
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <Text color="gray.500">Hello World!!</Text>
+        </div>
+    );
+};
+
+export default Home;
