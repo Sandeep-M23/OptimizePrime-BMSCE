6ca12dc00e4cf22d946fecb542613514c7ea3d9b
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2021-11-26 18:11:04 +0530

    Made Footer & NavBar

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import { Text } from '@chakra-ui/react';
+import { FunctionComponent } from 'react';
+
+const Footer: FunctionComponent = () => {
+    return (
+        <>
+            <Text>Footer</Text>
+        </>
+    );
+};
+
+export default Footer;
---
+++ components/Navigation/NavBar.tsx
@@ -0,0 +1,12 @@
+import { Text } from '@chakra-ui/react';
+import { FunctionComponent } from 'react';
+
+const NavBar: FunctionComponent = () => {
+    return (
+        <>
+            <Text>NavBar</Text>
+        </>
+    );
+};
+
+export default NavBar;
