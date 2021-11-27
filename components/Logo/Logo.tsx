51ee7ba1f31c50ff090482dc8c31e9ab276135c6
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2021-11-27 11:23:08 +0530

    Made Logo Component

---
+++ components/Logo/Logo.tsx
@@ -0,0 +1,19 @@
+import { Box, Image, Link } from '@chakra-ui/react';
+import { FunctionComponent } from 'react';
+
+const Logo: FunctionComponent = () => {
+    return (
+        <Box>
+            <Link href="/">
+                <Image
+                    src="/assets/images/logo.png"
+                    alt="Logo"
+                    maxW={'150px'}
+                    h={'auto'}
+                />
+            </Link>
+        </Box>
+    );
+};
+
+export default Logo;
