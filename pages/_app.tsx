ae36b6447d56aada387bc465fab057254bbc5e6f
Varun S Athreya <varun.sathreya@gmail.com>
2021-11-27 12:33:32 +0530

    Added selector css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { NextPage } from 'next';
 import { AppProps } from 'next/app';
+import '../css/styles.css';
 
 const App: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
     return (
