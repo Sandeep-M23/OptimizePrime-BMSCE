6603964bb19af0abae3827cbc7829f9ce3cc6de1
Varun S Athreya <varun.sathreya@gmail.com>
2021-11-27 11:06:29 +0530

    Changed images

--- components/Card/TestimonialCard.tsx
+++ components/Card/TestimonialCard.tsx
@@ -55,13 +55,7 @@ const TestimonialCard: FunctionComponent<Props> = ({ data }: Props) => {
                 </Text>
             </Stack>
             <Flex align={'center'} mt={8} direction={'column'}>
-                <Avatar
-                    src={
-                        'https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80'
-                    }
-                    alt={name}
-                    mb={2}
-                />
+                <Avatar src={imageUrl} alt={name} mb={2} />
                 <Stack spacing={-1} align={'center'}>
                     <Text fontWeight={600}>{name}</Text>
                     <Text
