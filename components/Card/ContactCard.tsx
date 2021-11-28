7188a53a4a6bf3019e355bb2660510f19c7cdfd5
Varun S Athreya <varun.sathreya@gmail.com>
2021-11-28 12:18:54 +0530

    Made contack form responsive

--- components/Card/ContactCard.tsx
+++ components/Card/ContactCard.tsx
@@ -2,7 +2,6 @@
 import {
     Box,
     Button,
-    Flex,
     FormControl,
     FormLabel,
     Input,
@@ -18,62 +17,61 @@ import { MdOutlineEmail } from 'react-icons/md';
 
 const ContactCard: FunctionComponent = () => {
     return (
-        <Flex>
-            <Box p={4}>
-                <WrapItem>
-                    <Box bg="white" borderRadius="lg">
-                        <Box m={8} color="#0B0E3F">
-                            <VStack spacing={5} width={'4xl'}>
-                                <FormControl id="name">
-                                    <FormLabel>Your Name</FormLabel>
-                                    <InputGroup borderColor="#E0E1E7">
-                                        <InputLeftElement
-                                            pointerEvents="none"
-                                            children={
-                                                <BsPerson color="gray.800" />
-                                            }
-                                        />
-                                        <Input type="text" size="lg" />
-                                    </InputGroup>
-                                </FormControl>
-                                <FormControl id="name">
-                                    <FormLabel>Mail</FormLabel>
-                                    <InputGroup borderColor="#E0E1E7">
-                                        <InputLeftElement
-                                            pointerEvents="none"
-                                            children={
-                                                <MdOutlineEmail color="gray.800" />
-                                            }
-                                        />
-                                        <Input type="text" size="lg" />
-                                    </InputGroup>
-                                </FormControl>
-                                <FormControl id="name">
-                                    <FormLabel>Message</FormLabel>
-                                    <Textarea
-                                        borderColor="gray.300"
-                                        _hover={{
-                                            borderRadius: 'gray.300',
-                                        }}
-                                        placeholder="message"
+        <Box p={4}>
+            <WrapItem>
+                <Box bg="white" borderRadius="lg">
+                    <Box m={8} color="#0B0E3F">
+                        <VStack
+                            spacing={5}
+                            width={{ lg: '3xl', md: 'xl', sm: 'xl' }}
+                        >
+                            <FormControl id="name">
+                                <FormLabel>Your Name</FormLabel>
+                                <InputGroup borderColor="#E0E1E7">
+                                    <InputLeftElement
+                                        pointerEvents="none"
+                                        children={<BsPerson color="gray.800" />}
                                     />
-                                </FormControl>
-                                <FormControl id="name" float="right">
-                                    <Button
-                                        variant="solid"
-                                        bg="#521262"
-                                        color="white"
-                                        _hover={{}}
-                                    >
-                                        Send Message
-                                    </Button>
-                                </FormControl>
-                            </VStack>
-                        </Box>
+                                    <Input type="text" size="lg" />
+                                </InputGroup>
+                            </FormControl>
+                            <FormControl id="name">
+                                <FormLabel>Mail</FormLabel>
+                                <InputGroup borderColor="#E0E1E7">
+                                    <InputLeftElement
+                                        pointerEvents="none"
+                                        children={
+                                            <MdOutlineEmail color="gray.800" />
+                                        }
+                                    />
+                                    <Input type="text" size="lg" />
+                                </InputGroup>
+                            </FormControl>
+                            <FormControl id="name">
+                                <FormLabel>Message</FormLabel>
+                                <Textarea
+                                    borderColor="gray.300"
+                                    _hover={{
+                                        borderRadius: 'gray.300',
+                                    }}
+                                    placeholder="message"
+                                />
+                            </FormControl>
+                            <FormControl id="name" float="right">
+                                <Button
+                                    variant="solid"
+                                    bg="#521262"
+                                    color="white"
+                                    _hover={{}}
+                                >
+                                    Send Message
+                                </Button>
+                            </FormControl>
+                        </VStack>
                     </Box>
-                </WrapItem>
-            </Box>
-        </Flex>
+                </Box>
+            </WrapItem>
+        </Box>
     );
 };
 
